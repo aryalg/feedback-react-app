e2e5d839f9ea7ac6baf9f09c4956b06efb09fb87
Bikram Aryal <bikramisdeveloper@gmail.com>
2022-04-11 20:49:37 +0545

    able to delete individual feedbacks

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import Card from './Card'
 import PropTypes from 'prop-types'
+import { FaTimes } from 'react-icons/fa';
 
-const FeedbackItem = ({ item }) => {
+const FeedbackItem = ({ item, handleDelete }) => {
   return (
     <Card>
       <div className='num-display' >{item.rating}</div>
+      <button onClick={() => handleDelete(item.id)} className='close'>
+        <FaTimes />
+      </button>
       <div className='text-display'>{item.text}</div>
     </Card>
   )
--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -5,16 +5,18 @@ import React from 'react'
 import FeedbackItem from './FeedbackItem';
 import PropTypes from 'prop-types'
 
-const FeedbackList = ({ feedback }) => {
+const FeedbackList = ({ feedback, handleDelete }) => {
 
   if (!feedback || feedback.length === 0) {
     return <p>No Feedback</p>
   }
 
+
+
   return (
     <div className='feedback-list'>
       {feedback.map((item) => (
-        < FeedbackItem key={item.id} item={item} />
+        < FeedbackItem key={item.id} item={item} handleDelete={handleDelete} />
       ))}
     </div>
   )
