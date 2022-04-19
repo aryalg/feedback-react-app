6ff4f258bb17fa6a3b9f113646c6ba895fb4b8b9
Bikram Aryal <bikramisdeveloper@gmail.com>
2022-04-19 20:31:34 +0545

    introducing animation using framer motion

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -4,6 +4,7 @@
 import React from 'react'
 import FeedbackItem from './FeedbackItem';
 import PropTypes from 'prop-types'
+import { AnimatePresence, motion } from 'framer-motion';
 
 const FeedbackList = ({ feedback, handleDelete, }) => {
 
@@ -14,11 +15,29 @@ const FeedbackList = ({ feedback, handleDelete, }) => {
 
 
   return (
-    <div className='feedback-list'>
+    <AnimatePresence>
+
       {feedback.map((item) => (
-        < FeedbackItem key={item.id} item={item} handleDelete={handleDelete} />
+
+        <motion.div
+          key={item.id}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+
+        >
+
+          < FeedbackItem key={item.id} item={item} handleDelete={handleDelete} />
+        </motion.div>
+
+
+
+
       ))}
-    </div>
+
+
+    </AnimatePresence>
+
   )
 }
 
